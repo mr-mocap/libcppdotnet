efc3d2e5c2463b4686c096f70f0c4afc612496da
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-09-30 14:53:57 -0400

    Reorganize & rename a few Modules/Partitions The project builds completely and the test can be run

--- tests/basics/driver.cpp
+++ tests/basics/driver.cpp
@@ -1,7 +1,13 @@
+import <iostream>;
+
 import System;
 
 int main ()
 {
-  using namespace std;
+    System::OverflowException of("Some kind of overflow");
 
+    std::cout << "Message = " << of.Message() << std::endl;
+    std::cout << "ToString() = " << of.ToString() << std::endl;
+    std::cout << "of.StackTrace() = " << of.StackTrace() << std::endl;
+    return 0;
 }
